4a5c287b1039dd4b769723534f9a366aaf94a7c2
David Volm <david@volminator.com>
2013-01-04 12:20:13 +0000

    Fixed make.

--- make.js
+++ make.js
@@ -1,4 +1,4 @@
 var UglifyJS = require('uglify-js'),
     fs = require('fs');
 
-fs.writeFileSync('./jsd.min.js', UglifyJS.minify('./jsd.js'));
+fs.writeFileSync('./jsd.min.js', UglifyJS.minify('./jsd.js').code);
